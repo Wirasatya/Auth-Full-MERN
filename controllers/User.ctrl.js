17eb892af3f0e6412f108d9abc68fb2b45a02db9
I Made Wira <wirasatya19@gmail.com>
2021-08-03 00:29:50 +0800

    commit: finished login, getAccessToken and forgotPass route

--- controllers/User.ctrl.js
+++ controllers/User.ctrl.js
@@ -7,7 +7,6 @@ const resMsg = require("../msgResponse");
 const { google } = require("googleapis");
 const { OAuth2 } = google.auth;
 const fetch = require("node-fetch");
-const { findOne } = require("../models/User.model");
 
 const { CLIENT_URL } = process.env;
 
@@ -75,6 +74,77 @@ const userCtrl = {
       res.json({ msg: resMsg.accountActivated }); // response success activation email
     } catch (err) {
       return res.status(500).json({ msg: err.message }); // something wrong on route user/activation
+    }
+  },
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body; // const email = req.body.email
+      const user = Users.findOne({ email }); //findOne match email from mongoDB
+      if (!user) return res.status(400).json({ msg: resMsg.emailNotExist }); //email not exist on mongoDB
+
+      const isMatch = await bcrypt.compare(password, user.password); //compare password from client and mongoDB
+      if (!isMatch) return res.status(400).json({ msg: resMsg.incorrectPass }); //password not match
+
+      const refresh_token = createRefreshToken({ id: user._id }); // create refresh token
+      res.cookie("refreshtoken", refresh_token, {
+        httpOnly: true,
+        path: "/user/refresh_token",
+        maxAge: 24 * 60 * 60 * 1000, // 1 days expired
+      }); // send refresh token to cookies
+
+      res.status(200).json({ msg: resMsg.loginSuccess }); //login success
+    } catch (err) {
+      return res.status(500).json({ msg: err.message }); // something wrong on route user/login
+    }
+  },
+  getAccessToken: (req, res) => {
+    try {
+      const rf_token = req.cookies.refreshtoken; // get refresh token from cookies
+      if (!rf_token) return res.status(400).json({ msg: resMsg.pleaseLogin }); // not refresh token on cookies
+
+      jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.status(400).json({ msg: resMsg.pleaseLogin }); // something wrong on refresh token
+
+        // console.log(user) //check value of decode refresh token
+        const access_token = createAccessToken({ id: user.id }); // encode user value to make access_token
+        res.json({ access_token }); // res value of access_token
+      });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message }); // something wrong on route user/accessToken
+    }
+  },
+  forgotPassword: async (req, res) => {
+    try {
+      const { email } = req.body; // set email value from req.body.email
+      const user = await Users.findOne({ email }); //findOne match email from mongoDB
+      if (!user)
+        return res.status(400).json({ msg: "This email does not exist." }); // email doesn't exist on mongodb
+
+      const access_token = createAccessToken({ id: user._id }); // create access_token
+      const url = `${CLIENT_URL}/user/reset/${access_token}`; // url reset password
+
+      sendEmail(email, url, "Reset your password"); // send email to user who forgot the password
+      res.json({ msg: "Re-send the password, please check your email." }); // response email abaut reset pass has been sent to his/her email
+    } catch (err) {
+      return res.status(500).json({ msg: err.message }); // something wrong on route user/resetPassword
+    }
+  },
+  resetPassword: async (req, res) => {
+    try {
+      const { password } = req.body; // set value of pass from req.body.password
+      // console.log(password); // check value of password
+      const passwordHash = await bcrypt.hash(password, 12); //hash the new password
+
+      await Users.findOneAndUpdate(
+        { _id: req.user.id },
+        {
+          password: passwordHash,
+        }
+      ); // find a user and update a password from mongodb
+
+      res.json({ msg: "Password successfully changed!" }); // a password successfully changed
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
     }
   },
 };
--- routes/User.route.js
+++ routes/User.route.js
@@ -7,4 +7,12 @@ router.post("/register", userCtrl.register);
 
 router.post("/activation", userCtrl.activateEmail);
 
+router.post("/login", userCtrl.login);
+
+router.post("/refresh_token", userCtrl.getAccessToken);
+
+router.post("/forgot", userCtrl.forgotPassword);
+
+router.post("/reset", auth, userCtrl.resetPassword);
+
 module.exports = router;
